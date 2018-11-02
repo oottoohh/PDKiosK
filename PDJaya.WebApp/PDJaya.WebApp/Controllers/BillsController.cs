12b2871e1560e405f209e51b447a4c7fb475f233
galihyprtm <galihyprtm@gmail.com>
2018-11-02 15:40:14 +0700

    Tambah CRUD page Bills

---
+++ PDJaya.WebApp/PDJaya.WebApp/Controllers/BillsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PDJaya.WebApp.Models;
+using PDJaya.WebApp.Helper;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace PDJaya.WebApp.Controllers
+{
+    public class BillsController : Controller
+    {
+        HelperClientHttp _api = new HelperClientHttp();
+        public async Task<IActionResult> BillsView()
+        {
+            List<Bills> Bills = new List<Bills>();
+            HttpClient client = await _api.InitialAsync(); //dari Global Variable
+            HttpResponseMessage res = await client.GetAsync("/api/Bills"); //URL GET DATA ke UserProfile check di https://pdjayaservice.azurewebsites.net
+            if (res.IsSuccessStatusCode)
+            {
+                var result = await res.Content.ReadAsStringAsync();
+                var output = JsonConvert.DeserializeObject<OutputData>(result); //Output data di Helper fungsi untuk menampung data sementara
+                if (output != null && output.IsSucceed)
+                {
+                    Bills = ((JArray)output.Data).ToObject<List<Bills>>(); // Pass data dari output.Data ke list userprofile class
+                }
+            }
+            return View(Bills);
+        }
+
+        public IActionResult CreateBills()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind] Bills dtForm)
+        {
+            if (ModelState.IsValid)
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(dtForm), Encoding.UTF8, "application/json");
+                HttpClient client = await _api.InitialAsync();
+                var result = await client.PostAsync("/api/Bills", content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(resultContent);
+            }
+            return RedirectToAction(nameof(BillsView));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            List<Bills> Bills = new List<Bills>();
+            HttpClient client = await _api.InitialAsync();
+            HttpResponseMessage res = await client.GetAsync("/api/Bills/" + id);
+            var result = await res.Content.ReadAsStringAsync();
+            var o = JObject.Parse(result);
+            var data = JsonConvert.DeserializeObject<Bills>(o.Root.ToString());
+            return View(data);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int? id, [Bind] Bills dtForm)
+        {
+            if(ModelState.IsValid)
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(dtForm), Encoding.UTF8, "application/json");
+                HttpClient client = await _api.InitialAsync();
+                var result = await client.PutAsync("/api/Bills/"+id, content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(resultContent);
+            }
+            return RedirectToAction(nameof(BillsView));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            List<Bills> Bills = new List<Bills>();
+            HttpClient client = await _api.InitialAsync();
+            HttpResponseMessage res = await client.GetAsync("/api/Bills/" + id);
+            var result = await res.Content.ReadAsStringAsync();
+            var o = JObject.Parse(result);
+            var data = JsonConvert.DeserializeObject<Bills>(o.Root.ToString());
+            return View(data);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            HttpClient client = await _api.InitialAsync();
+            HttpResponseMessage res = await client.DeleteAsync("/api/Bills/" + id);
+            string resultContent = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(resultContent);
+            return RedirectToAction("BillsView");
+        }
+    }
+}
---
+++ PDJaya.WebApp/PDJaya.WebApp/Models/Bills.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace PDJaya.WebApp.Models
+{
+    public partial class Bills
+    {
+        public long Id { get; set; }
+        public string BillID { get; set; }
+        public string MarketNo { get; set; }
+        public string StoreNo { get; set; }
+        public string TransactionCode { get; set; }
+        public decimal Amount { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+    }
+}
