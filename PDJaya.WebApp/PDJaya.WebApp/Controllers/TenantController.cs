e073fcc6c49b0ce8315c3bd31208cb006f901446
galihyprtm <galihyprtm@gmail.com>
2018-11-03 09:37:29 +0700

    tambah CRUD Tenant

---
+++ PDJaya.WebApp/PDJaya.WebApp/Controllers/TenantController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PDJaya.WebApp.Models;
+using PDJaya.WebApp.Helper;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace PDJaya.WebApp.Controllers
+{
+    public class TenantController : Controller
+    {
+        HelperClientHttp _api = new HelperClientHttp();
+        public async Task<IActionResult> TenantView()
+        {
+            List<Tenant> Tenant = new List<Tenant>();
+            HttpClient client = await _api.InitialAsync(); 
+            HttpResponseMessage res = await client.GetAsync("/api/Tenants"); 
+            if (res.IsSuccessStatusCode)
+            {
+                var result = await res.Content.ReadAsStringAsync();
+                var output = JsonConvert.DeserializeObject<OutputData>(result);
+                if (output != null && output.IsSucceed)
+                {
+                    Tenant = ((JArray)output.Data).ToObject<List<Tenant>>();
+                }
+            }
+            return View(Tenant); 
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind] Tenant dtForm)
+        {
+            if (ModelState.IsValid)
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(dtForm), Encoding.UTF8, "application/json");
+                HttpClient client = await _api.InitialAsync();
+                var result = await client.PostAsync("/api/Tenants", content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(resultContent);
+            }
+            return RedirectToAction(nameof(TenantView));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            List<Tenant> Tenant = new List<Tenant>();
+            HttpClient client = await _api.InitialAsync();
+            HttpResponseMessage res = await client.GetAsync("/api/Tenants/" + id);
+            var result = await res.Content.ReadAsStringAsync();
+            var o = JObject.Parse(result);
+            var data = JsonConvert.DeserializeObject<Tenant>(o.Root.ToString());
+            return View(data);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int? id, [Bind] Tenant dtForm)
+        {
+            if (ModelState.IsValid)
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(dtForm), Encoding.UTF8, "application/json");
+                HttpClient client = await _api.InitialAsync();
+                var result = await client.PutAsync("/api/Tenants/" + id, content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(resultContent);
+            }
+            return RedirectToAction(nameof(TenantView));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            List<Tenant> Tenant = new List<Tenant>();
+            HttpClient client = await _api.InitialAsync();
+            HttpResponseMessage res = await client.GetAsync("/api/Tenants/" + id);
+            var result = await res.Content.ReadAsStringAsync();
+            var o = JObject.Parse(result);
+            var data = JsonConvert.DeserializeObject<Tenant>(o.Root.ToString());
+            return View(data);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            HttpClient client = await _api.InitialAsync();
+            HttpResponseMessage res = await client.DeleteAsync("/api/Tenants/" + id);
+            string resultContent = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(resultContent);
+            return RedirectToAction("TenantView");
+        }
+
+    }
+}
---
+++ PDJaya.WebApp/PDJaya.WebApp/Models/Tenant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDJaya.WebApp.Models
+{
+    public partial class Tenant
+    {
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public System.DateTime Created { get; set; }
+        public System.DateTime Updated { get; set; }
+        public long Id { get; set; }
+        public string StoreNo { get; set; }
+        public string Owner { get; set; }
+        public string Remark { get; set; }
+        public int Status { get; set; }
+        public string MarketNo { get; set; }
+    }
+}
